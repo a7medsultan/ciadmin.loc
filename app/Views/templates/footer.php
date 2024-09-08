f28c8f6c047ca2afa6f3da4cb44cebd830319ff3
Ahmed Sultan <10511308+a7medsultan@users.noreply.github.com>
2024-09-08 15:02:34 +0400

    added adminlte template

---
+++ app/Views/templates/footer.php
@@ -0,0 +1,41 @@
+<!-- form modal -->
+<div class="modal fade" id="form_modal">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 id="form_title" class="modal-title"></h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div id="form_fields" class="modal-body">
+                <p>loading the form <i class="fas fa-spinner fa-spin"></i></p>
+            </div>
+
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+
+
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+
+<!-- Main Footer -->
+<footer class="main-footer">
+    <strong>Copyright &copy; <?= date('Y') ?>.</strong>
+    All rights reserved.
+</footer>
+</div>
+<!-- ./wrapper -->
+
+
+
+
+</body>
+</html>
